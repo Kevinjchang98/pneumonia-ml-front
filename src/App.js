3332333a1e4501dc6aab44294d7b2fd01a491f37
Kevin Chang <kevinjchang98@gmail.com>
2020-10-27 14:02:40 -0700

    minimal working file upload to local server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+	const [ file, setFile ] = useState();
+	const fileSelectedHandler = (event) => {
+		console.log(event.target.files[0]);
+		setFile(event.target.files[0]);
+	};
+
+	const fileUploadHandler = () => {
+		console.log('upload button clicked');
+		const fd = new FormData();
+		fd.append('image', file, file.name);
+		axios.post('http://127.0.0.1:8000/predict').then((res) => {
+			console.log(res);
+		});
+	};
+
 	return (
 		<div className="App">
-			<h1>Test</h1>
+			<h1>Header</h1>
+			<input type="file" onChange={fileSelectedHandler} />
+			<button onClick={fileUploadHandler}>Upload</button>
 		</div>
 	);
 }
